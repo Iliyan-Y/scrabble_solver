29b341f09194fe3059d404f65e3a3dc90649b85b
Iliyan-Y <ili.yankov91@gmail.com>
2020-11-27 13:33:57 +0000

    First word test passed

--- spec/scrabbleSolverSpec.js
+++ spec/scrabbleSolverSpec.js
@@ -15,4 +15,9 @@ describe('ScrabbleSolver', () => {
     solver = new ScrabbleSolver('f');
     expect(solver.score()).toEqual(4);
   });
+
+  it('It retunrs score of zero when the word is empty', () => {
+    solver = new ScrabbleSolver('street');
+    expect(solver.score()).toEqual(6);
+  });
 });
